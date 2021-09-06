dad2d95b5cd635a1a7d862f50656dca94c93d2c4
袁仕崇 <xiaochong2154@163.com>
2021-09-06 16:09:42 +0800

    ADD: // enemies and hurt animation

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -21,6 +22,11 @@ public class PlayerControl : MonoBehaviour
 
     public int cherry; //count
 
+    public Text cherryNumberText;
+
+    //受伤中，不需要执行滑动
+    private bool isHurt;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +43,10 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Movement();
+        if (!isHurt)
+        {
+            Movement();
+        }
         SwitchAnimation();
     }
 
@@ -62,8 +71,9 @@ public class PlayerControl : MonoBehaviour
         }
 
         //角色跳跃
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && collider2D.IsTouchingLayers(ground)) {
             //给 Y 轴设置跳跃力
+
             rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
 
             //设置跳跃值
@@ -75,26 +85,81 @@ public class PlayerControl : MonoBehaviour
     void SwitchAnimation()
     {
         //正在跳跃
-        if (animator.GetBool("jumping")) {
+        if (animator.GetBool("jumping"))
+        {
             ////如果 Y 轴设置跳跃力消失
             if (rb.velocity.y < 0)
             {
                 animator.SetBool("jumping", false);
                 animator.SetBool("falling", true);
             }
-        } else if (collider2D.IsTouchingLayers(ground)) { //当狐狸碰撞到地面
-            animator.SetBool("falling", false); 
+        }
+        else if (isHurt) {
+
+            animator.SetFloat("running", 0);
+
+            if (Mathf.Abs(rb.velocity.x) < 0.1f)
+            {
+                animator.SetBool("hurting", false);
+                animator.SetBool("idle", true); 
+                isHurt = false;
+            } else
+            {
+                
+            }
+        }
+        else if (collider2D.IsTouchingLayers(ground))
+        { //当狐狸碰撞到地面
+            animator.SetBool("falling", false);
             animator.SetBool("idle", true);
         }
+        //
     }
 
-    //当碰到收集类的tag 如：樱桃等
+    //当狐狸碰到收集类的tag 如：樱桃等
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Collection")
         {
             Destroy(collision.gameObject);
             cherry += 1;
+            cherryNumberText.text = cherry.ToString();
         }
     }
+
+    //消灭敌人
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemies") {
+            if (animator.GetBool("falling")) {
+                Destroy(collision.gameObject);
+
+
+                //
+                //float scale = 0.4;
+                //踏上之后加个小跳
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime * 0.4f);
+
+                //设置跳跃值
+                animator.SetBool("jumping", true);
+            }
+            else
+            {
+                isHurt = true;
+                animator.SetBool("hurting", true);
+                if (transform.position.x < collision.gameObject.transform.position.x)
+                {
+                    //碰到敌人向左移10
+                    rb.velocity = new Vector2(-5, rb.velocity.y);
+                    
+                }
+                else if (transform.position.x > collision.gameObject.transform.position.x)
+                {
+                    //碰到敌人向右移10
+                    rb.velocity = new Vector2(5, rb.velocity.y);
+                }
+            }
+        }
+    }
+
 }
